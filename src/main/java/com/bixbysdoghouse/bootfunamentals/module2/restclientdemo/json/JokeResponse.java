7343103636d211bb8f772ca172aed0cb1e87e437
Dave Carlson <dave.carlson@tr.com>
2019-11-07 13:39:55 -0600

    Switching over to using Lombok.  So much nicer.

--- src/main/java/com/bixbysdoghouse/bootfunamentals/module2/restclientdemo/json/JokeResponse.java
+++ src/main/java/com/bixbysdoghouse/bootfunamentals/module2/restclientdemo/json/JokeResponse.java
@@ -1,54 +1,13 @@
 package com.bixbysdoghouse.bootfunamentals.module2.restclientdemo.json;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class JokeResponse {
     private String status;
     private Value value;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Value getValue() {
-        return value;
-    }
-
-    public void setValue(Value value) {
-        this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        JokeResponse that = (JokeResponse) o;
-        return Objects.equals(status, that.status) &&
-                Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(status, value);
-    }
-
-    @Override
-    public String toString() {
-        return "JokeResponse{" +
-                "status='" + status + '\'' +
-                ", value=" + value +
-                '}';
-    }
-
     public String getJoke() {
-        String result = "";
-        if (value != null) {
-            result = value.getJoke();
-        }
-        return result;
+        return value.getJoke();
     }
 }
--- src/main/java/com/bixbysdoghouse/bootfunamentals/module2/restclientdemo/json/Value.java
+++ src/main/java/com/bixbysdoghouse/bootfunamentals/module2/restclientdemo/json/Value.java
@@ -1,58 +1,11 @@
 package com.bixbysdoghouse.bootfunamentals.module2.restclientdemo.json;
 
 import java.util.Collection;
-import java.util.Objects;
+import lombok.Data;
 
-public class Value {
+@Data
+class Value {
     private int id;
     private String joke;
     private Collection<String> categories;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    String getJoke() {
-        return joke;
-    }
-
-    public void setJoke(String joke) {
-        this.joke = joke;
-    }
-
-    public Collection<String> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Collection<String> categories) {
-        this.categories = categories;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Value value = (Value) o;
-        return id == value.id &&
-                Objects.equals(joke, value.joke) &&
-                Objects.equals(categories, value.categories);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, joke, categories);
-    }
-
-    @Override
-    public String toString() {
-        return "Value{" +
-                "id=" + id +
-                ", joke='" + joke + '\'' +
-                ", categories=" + categories +
-                '}';
-    }
 }
